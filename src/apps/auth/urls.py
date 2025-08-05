5fb365031eff8991cdbff20657003edcba335a8a
Prem Poudel <prempoudelcsit079@chitwancollege.edu.np>
2025-08-05 16:15:36 +0545

    feat/auth: urls added

--- src/apps/auth/urls.py
+++ src/apps/auth/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-
+from .views import (
+    UserRegisterView,
+    UserLoginView,
+    UserProfileView,
+    UserLogoutView,
+)
 
 
 urlpatterns = [
-    
+    path("register/", UserRegisterView.as_view(), name="user-register"),
+    path("login/", UserLoginView.as_view(), name="user-login"),
+    path("profile/", UserProfileView.as_view(), name="user-profile"),
+    path("logout/", UserLogoutView.as_view(), name="user-logout"),
 ]
