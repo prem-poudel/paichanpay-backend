254d9c8ed18cffe84868cc26e63c29cdc1208e48
Prem Poudel <prempoudelcsit079@chitwancollege.edu.np>
2025-08-05 14:10:45 +0545

    feat/auth: models created

--- src/settings.py
+++ src/settings.py
@@ -141,6 +141,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "authentication.User"
 
 # CORS settings
 CORS_ALLOWED_ORIGINS = config("CORS_ALLOWED_ORIGINS", default="*", cast=Csv())
